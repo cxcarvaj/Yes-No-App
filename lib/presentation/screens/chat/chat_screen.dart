0d93014bfa6461a19f9d71213c7a7d4e28ccb58e
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-06-24 12:55:50 -0500

    Creating the chat screen and adding the reply message bubble widget

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 
 // If the widget is a Screen, then you should return a Scaffold.
@@ -16,7 +17,9 @@ class ChatScreen extends StatelessWidget {
                 'https://instagram.fgye4-1.fna.fbcdn.net/v/t51.2885-15/355228769_280923334510038_3465071754206350459_n.jpg?stp=dst-jpg_e35_p720x720&_nc_ht=instagram.fgye4-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=WW1tYCea-ZAAX-hVu6r&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzEzMTA0NjM4NzI4MzE0NzA4OA%3D%3D.2-ccb7-5&oh=00_AfDfCtBGAJ2L4UXfdaeWEqBwpP5DLCtyUOqQ0obtaMm_cg&oe=649CBF79&_nc_sid=ee9879'),
           ),
         ),
-        title: const Text('Chat with bae 💕'),
+        // title: const Text('Chat with bae 💕'),
+        title: const Text('Hailee Steinfeld 💕'),
+
         centerTitle: false,
       ),
       body: _ChatView(),
@@ -42,7 +45,9 @@ class _ChatView extends StatelessWidget {
               child: ListView.builder(
                 itemCount: 100,
                 itemBuilder: (context, index) {
-                  return const MyMessageBubble();
+                  return (index % 2 == 0)
+                      ? const HerMessageBubble()
+                      : const MyMessageBubble();
                 },
               ),
             ),
